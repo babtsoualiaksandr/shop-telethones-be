849b3e47279f70579ff873157a6e14218b5cbc4b
Aliaksandr Babtsou <babtsoualiaksandr@gmail.com>
2020-11-05 15:25:22 +0300

    feature: implement POST/products lambda functions returns error 400 status code if product data is invalid

--- product-service/postProducts.js
+++ product-service/postProducts.js
@@ -1,23 +1,34 @@
 // eslint-disable-next-line import/prefer-default-export
 // import products from './data/products';
 import pg_client from './data/pg_client';
+import { patternProduct } from './Models/insertProducts';
+const jpv = require('jpv');
 
 export async function post(event) {
-  const products = event.pathParameters.products;
-  console.log(products)
-  const product = await pg_client.postProducts(products);
-  if (product === undefined) {
-    return {
-      statusCode: 404,
-      body: JSON.stringify({error: 'Not found'}),
-  };
-  }
+    const products = event.pathParameters.products;
+    console.log(products);
+
+    const validRequest = jpv.validate(products, patternProduct);
+    console.log(validRequest);
+    if (validRequest !== true) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error: 'product data is invalid' }),
+        };
+    }
+    const product = await pg_client.postProducts(products);
+    if (product === undefined) {
+        return {
+            statusCode: 404,
+            body: JSON.stringify({ error: 'Not found' }),
+        };
+    }
     return {
         statusCode: 200,
         headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Credentials': true,
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
         },
-        body: JSON.stringify({product}),
+        body: JSON.stringify({ product }),
     };
 }
