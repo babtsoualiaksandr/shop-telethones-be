d6129b1e22a22fb6d51c62ee108a0764e223a9a8
Aliaksandr Babtsou <babtsoualiaksandr@gmail.com>
2020-12-12 09:21:58 +0300

    fiat:  Added functionality for caching requests

--- bff-service/server.js
+++ bff-service/server.js
@@ -7,50 +7,67 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
-app.all('/*', (req, res) => {
+const cache = {};
+const CASHTIME = 1 * 1000 * 60;
+const midWare = (req, res, next) => {
+    const key = req.url;
+    const now = new Date();
+    if (cache[key] && now - cache[key].timeCache < CASHTIME) {
+        console.log(cache);
+        res.header('Access-Control-Allow-Origin', '*');
+        res.send(`from cache ${cache[key].body}`);
+    } else {
+        res.sendResponse = res.send;
+        res.send = (body) => {
+            const datetime = new Date();
+            cache[key] = { timeCache: datetime, body: body };
+            res.sendResponse(body);
+        };
+        next();
+    }
+};
 
+app.all('/*', midWare, (req, res) => {
     console.log('originalUrl', req.originalUrl);
     console.log('method', req.method);
-    console.log('body',req.body);
+    console.log('body', req.body);
 
     const recipient = req.originalUrl.split('/')[1];
 
     const recipientURL = process.env[recipient];
-    console.log('recipientURL',recipientURL);
+    const urlRe = req.originalUrl.split('/').splice(2, 10).join('/');
+    console.log('req.originalUrl ========= ', urlRe);
+    console.log('recipientURL', recipientURL);
+    console.log('', `${recipientURL}${urlRe}`);
 
     if (recipientURL) {
         const axiosConfig = {
             method: req.method,
-            url: `${recipientURL}`,
-            ...(Object.keys(req.body || {}).length > 0 && {data: req.body})
+            url: `${recipientURL}/${urlRe}`,
+            ...(Object.keys(req.body || {}).length > 0 && { data: req.body }),
         };
         console.log('axiosConfig', axiosConfig);
 
         axios(axiosConfig)
             .then(function (response) {
                 console.log('response from ', response.data);
-                res.json(response.data);             
+                res.json(response.data);
             })
-            .catch(error => {
-                console.log('error', JSON.stringify(error))
+            .catch((error) => {
+                console.log('error', JSON.stringify(error));
                 if (error.response) {
-                    const {
-                        status,
-                        data
-                    } = error.response;
+                    const { status, data } = error.response;
 
                     res.status(status).json(data);
                 } else {
-                    res.status(500).json({error: error.message});
+                    res.status(500).json({ error: error.message });
                 }
             });
     } else {
-        res.status(502).json({error: 'Cannot  process request'});
+        res.status(502).json({ error: 'Cannot  process request' });
     }
-
 });
 
 app.listen(PORT, () => {
     console.log(`Example app listening at localhost:${PORT}`);
-})
-
+});
